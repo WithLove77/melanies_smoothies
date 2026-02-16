2fdd4de1275a9807c709250b912c307402897c68
WithLove77 <pramod@snelder.de>
2026-02-16 13:11:23 +0100

    Update streamlit_app.py
    debugging error

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,7 @@ if ingredients_list:
         
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.write("The search string therefore is: https://my.smoothiefroot.com/api/fruit/{search_on}")
       
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on}") 
         #st.text(smoothiefroot_response.json()) 
